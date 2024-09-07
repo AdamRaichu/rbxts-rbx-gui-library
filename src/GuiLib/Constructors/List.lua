ed82d87a97c6f5cb2d2c7ac28c461b51e61e5d70
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2024-09-07 02:03:54 -0400

    add def for List

--- src/GuiLib/Constructors/List.lua
+++ src/GuiLib/Constructors/List.lua
@@ -8,7 +8,7 @@ This module only has one function:
 List.Create(
 	string[] list, -- an array of strings used to generate instances for the list
 	integer scrollMax, -- the maximum number of elements visible in the scroll frame at any given time. Defaults to math.huge
-	enum fillDirection, -- Enum.FillDirection lets you decide if the list is vertical or horizontal. Defaults to verical
+	enum fillDirection, -- Enum.FillDirection lets you decide if the list is vertical or horizontal. Defaults to vertical
 	udim padding, -- scale and offset for padding between instances. Defaults to no padding
 	function instanceFunc -- must return a UI object such as a frame. Defaults to creating a textbutton with a child textlabel
 )
